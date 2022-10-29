f35cf5f1eee892c34333267983327180e411cdda
Woong <jiyko7@kyonggi.ac.kr>
2022-10-29 14:23:05 +0900

    젤리 키우기 게임
    클론코딩

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Sprite[] JellySpriteList;
+    public string[] JellyNameList;
+    public int[] JellyJelatinList;
+    public int[] JellyGoldList;
+    public int[] NumGoldList;
+    public int[] ClickGoldList;
+
+    public Vector3[] PointList;
+
+    public RuntimeAnimatorController[] LevelAc;
+
+    public void ChangeAc(Animator anim, int level)
+    {
+        anim.runtimeAnimatorController = LevelAc[level - 1];
+    }
+}
